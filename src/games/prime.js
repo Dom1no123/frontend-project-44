2a0d97362541f1270f4020e3c5ce879a2c96a7b7
Martin <gangshit9270@gmail.com>
2024-10-27 20:00:11 +0400

    Debug prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,17 @@ import runGame from '../index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+// eslint-disable-next-line consistent-return
 const correctSolution = (num) => {
-  if (num % 2 === 0 || num % 3 === 0 || num % 5 === 0 || num % 7 === 0) {
-    return false;
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
   }
-  return true;
 };
 
 const generateGameData = () => {
@@ -21,4 +27,5 @@ const generateGameData = () => {
 const runPrime = () => {
   runGame(gameDescription, generateGameData);
 };
+
 export default runPrime;
