262d7f6361188997a6e72f9d5d21e9eb3474b04c
Martin <gangshit9270@gmail.com>
2024-10-27 20:47:47 +0400

    Debug linter

--- src/games/games.js
+++ src/games/games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import greeting from '../cli';
+// eslint-disable-next-line import/extensions
+import greeting from '../cli.js';
 
 console.log('Welcome to the Brain Games!');
 greeting();
--- src/games/prime.js
+++ src/games/prime.js
@@ -4,20 +4,20 @@ import runGame from '../index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-// eslint-disable-next-line consistent-return
+// Проверка, является ли число простым
 const correctSolution = (num) => {
-    if (num <= 1) return false; 
-    if (num <= 3) return true; 
-    if (num % 2 === 0 || num % 3 === 0) return false; 
-  
-    for (let i = 5; i * i <= num; i += 6) {
-      if (num % i === 0 || num % (i + 2) === 0) {
-        return false; 
-      }
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
     }
-    
-    return true; 
-  };
+  }
+  return true; // Добавлено для завершения функции
+};
+
 const generateGameData = () => {
   const randomNum = Math.floor(Math.random() * 100) + 1;
   const question = randomNum;
